5f98bad8e7f541b3ee40fb4f43da747fbf1d237d
Maksym <maxim.golya@gmail.com>
2026-04-16 22:10:57 +0300

    unfinished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,146 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tBody = table.querySelector('tbody');
+
+let currentIndex = null;
+let direction = 'asc';
+
+function sortTable() {
+  table.addEventListener('click', (e) => {
+    const th = e.target.closest('th');
+
+    if (th) {
+      const index = th.cellIndex;
+
+      if (currentIndex === index) {
+        direction = direction === 'asc' ? 'desc' : 'asc';
+      } else {
+        currentIndex = index;
+        direction = 'asc';
+      }
+
+      const rows = [...tBody.querySelectorAll('tr')];
+
+      rows.sort((rowA, rowB) => {
+        const cellA = rowA.children[index];
+        const cellB = rowB.children[index];
+        const valA = cellA.textContent;
+        const valB = cellB.textContent;
+        const cleanA = valA.replace(/[^0-9]/g, '');
+        const cleanB = valB.replace(/[^0-9]/g, '');
+        const numA = Number(cleanA);
+        const numB = Number(cleanB);
+
+        if (cleanA !== '' && cleanB !== '') {
+          return direction === 'asc' ? numA - numB : numB - numA;
+        } else {
+          return direction === 'asc'
+            ? valA.localeCompare(valB)
+            : valB.localeCompare(valA);
+        }
+      });
+      tBody.append(...rows);
+
+      return;
+    }
+
+    function selectRow() {
+      const row = e.target.closest('tr');
+
+      if (!row) {
+        return;
+      }
+
+      if (!tBody.contains(row)) {
+        return;
+      }
+
+      const rows = [...tBody.querySelectorAll('tr')];
+
+      rows.forEach(() => {
+        row.classList.remove('active');
+      });
+      row.classList.add('active');
+    }
+    selectRow();
+  });
+}
+sortTable();
+
+function createForm() {
+  const form = document.createElement('form');
+
+  form.classList.add('new-employee-form');
+  table.after(form);
+
+  const label = document.createElement('label');
+
+  label.textContent = 'Name: ';
+
+  const input = document.createElement('input');
+
+  input.name = 'name';
+  input.type = 'text';
+  input.dataset.qa = 'name';
+
+  label.append(input);
+  form.append(label);
+
+  const labelPosition = document.createElement('label');
+
+  labelPosition.textContent = 'Position: ';
+
+  const inputPosition = document.createElement('input');
+
+  inputPosition.name = 'position';
+  inputPosition.type = 'text';
+  inputPosition.dataset.qa = 'position';
+
+  labelPosition.append(inputPosition);
+  form.append(labelPosition);
+
+  const labelAge = document.createElement('label');
+
+  labelAge.textContent = 'Age: ';
+
+  const inputAge = document.createElement('input');
+
+  inputAge.name = 'age';
+  inputAge.type = 'number';
+  inputAge.dataset.qa = 'age';
+
+  labelAge.append(inputAge);
+  form.append(labelAge);
+
+  const labelSalary = document.createElement('label');
+
+  labelSalary.textContent = 'Salary: ';
+
+  const inputSalary = document.createElement('input');
+
+  inputSalary.name = 'salary';
+  inputSalary.type = 'number';
+  inputSalary.dataset.qa = 'salary';
+
+  labelSalary.append(inputSalary);
+  form.append(labelSalary);
+
+  const button = document.createElement('button');
+
+  button.type = 'submit';
+  button.textContent = 'Save to table';
+
+  form.append(button);
+
+  form.addEventListener('submit', handleSubmit);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // const formData = new FormData(e.target);
+    // const name = formData.get('name');
+    // const position = formData.get('position');
+  }
+}
+createForm();
